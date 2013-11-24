2855a276a400225389730985b79b780f642506b0
Nick L <nicklonz@gmail.com>
2013-11-24 13:24:17 -0500

    Added Exercises

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+# puts "This line won't print."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd rather you 'not' do this to me Nick."
+puts 'I "said" do not do this to me Nick.'
---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# An octothorpe in fornt of a line tells Ruby to ignore it
+
+puts "I could have code like this. " #and an ignored comment after
+
+# You can also use a comment to "disable" or comment out a line
+
+# puts "This won't run."
+
+puts "This will run."
