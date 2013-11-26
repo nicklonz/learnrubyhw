80221af01a052b9151d477464278020337dd28a7
Nick L <nicklonz@gmail.com>
2013-11-26 00:03:18 -0500

    Added Sample Exercise

---
+++ sample1.rb
@@ -0,0 +1,16 @@
+puts "What's your name? " 
+name = gets.chomp.capitalize
+puts "Hello #{name}, what state do you live in? "
+state = gets.chomp.upcase
+puts "#{state} is a wonderful place #{name}!"
+puts "What is your street address #{name}? "
+address = gets.chomp.upcase
+puts "What city/town/village do you live in #{state} #{name}?"
+city = gets.chomp.upcase
+puts "One more thing #{name}, what is your Zip code?"
+zipcode = gets.chomp
+puts "OK, great #{name}. Your maling address is: "
+puts ""
+puts "#{name}"
+puts "#{address}"
+puts "#{city}, #{state} #{zipcode}"
